2cc8cf4648c8a4d2af2bb2bb66546b1a9380ad33
PrabhatYadav-27 <prabhaty2727@gmail.com>
2024-11-18 14:10:43 +0530

    added the skeleton routes of

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+
+
+
+// creating the endpoint or routes 
+
+//endpoint for signup
+app.post("/user/signup", function(req,res){
+
+
+});
+
+//endpoint for signin
+app.post("/user/signin", function(req,res){
+
+
+})
+
+
+//endpoint for list of  all purchases
+app.get("user/purchses", function(req,res){
+
+
+});
+
+//endpoint for purchasing the course
+app.post("/course/purchase", function(req,res){
+
+
+});
+
+//endpoint for all the courses on the app
+app.get("/courses", function(req,res){
+
+
+});
+
+
+
+
+
+app.listen(3000); //listennig on port 3000
