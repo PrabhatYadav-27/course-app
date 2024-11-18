806325ac04cf8d465080c68b2ecc30967fed16a2
PrabhatYadav-27 <prabhaty2727@gmail.com>
2024-11-18 15:09:35 +0530

    added the admin skeleton routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 
 const app = express();
 
@@ -8,7 +9,9 @@ const app = express();
 app.use("/user", userRouter);
 
 //for future api and versioning above codes acts like below one
-// app.use("/api/v1/user", userRouter)  
+// app.use("/api/v1/user", userRouter)
+app.use("/admin", adminRouter);
+
 app.use("/course", courseRouter);
 
 
---
+++ routes/admin.js
@@ -0,0 +1,52 @@
+const {Router} = require("express");
+
+const adminRouter = Router();
+
+//Routes for admin
+
+
+//signup endpoint for admin
+adminRouter.post("/signup", function(req,res){
+    res.json({
+        message: "admin signup enpoint"
+    })
+});
+
+//signin endpoint for admin
+
+adminRouter.post("/signin", function(req,res){
+    res.json({
+        message: "admin signin endpoint"
+    })
+});
+
+//course endpoint for admin
+
+adminRouter.post("/course", function(req,res){
+    res.json({
+        message: "admin course endpoint"
+    })
+});
+
+//change course endpoint for admin
+
+adminRouter.put("/course", function(req,res){
+    res.json({
+        message: "admin put  course endpoint"
+    })
+});
+
+//view all course endpoint for admin
+
+
+adminRouter.get("/course/all-course", function(req,res){
+    res.json({
+        message: "admin all course endpoint"
+    })
+});
+
+//
+
+module.exports = {
+    adminRouter: adminRouter
+}
